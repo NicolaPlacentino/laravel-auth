c2ca5fca58e4e69242f5e839b38a15ba96374a9e
NicolaPlacentino <nyko0921@gmail.com>
2023-03-07 18:27:09 +0100

    twicked graphic

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col">
+        <div class="col p-5">
             <table class="table">
                 <thead>
                   <tr>
