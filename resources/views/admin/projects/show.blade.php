d99c9c7dc1221c9df9899e7a14faf34e043c9374
NicolaPlacentino <nyko0921@gmail.com>
2023-03-09 15:59:10 +0100

    added image graphic in show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,15 +4,27 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-auto p-5">
-            <div class="card" style="width: 20rem;">
-                <div class="card-header">
-                  <b>Nome progetto:</b><br>{{$project->name}}
+            <div class="card overflow-hidden" style="width: @if($project->image) 32rem; @else 20rem; @endif ">
+              <div class=" d-flex">
+                <div class="@if($project->image) w-50 @else w-100 @endif">
+                  <div class="card-header">
+                    <b>Nome progetto:</b><br>{{$project->name}}
+                  </div>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><b>Data di completamento:</b><br>{{$project->completion_date}}</li>
+                    <li class="list-group-item"><b>Autore:</b><br>{{$project->author}}</li>
+                  </ul>
                 </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item"><b>Data di completamento:</b><br>{{$project->completion_date}}</li>
-                  <li class="list-group-item"><b>Autore:</b><br>{{$project->author}}</li>
-                </ul>
+                @if ($project->image)
+                <div class="w-50">
+                  <figure class="m-0 p-0 w-100 h-100">
+                    <img src="..." 
+                    class="object-fit-cover object-position-center rounded-end" alt="...">
+                  </figure>
+                </div>
+                @endif
               </div>
+            </div>
         </div>
     </div>
     <div class="text-center">
